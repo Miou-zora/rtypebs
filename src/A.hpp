6e5976dc9fe1c1be8e785448b2ea0c1fe81cafb7
foxaxe <axel.idoux@epitech.eu>
2023-09-25 17:37:13 +0200

    test: add some fake test to show how it works

---
+++ src/A.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** NanoTekSpice
+** File description:
+** AComponents
+*/
+
+#include <iostream>
+
+class A
+{
+    public:
+        A() = default;
+        ~A() = default;
+
+        size_t addition(size_t a, size_t b)
+        {
+            return a + b;
+        }
+};
